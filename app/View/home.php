4a1542bf6990cd7603115594c232ac6d21033829
koji.iwase <koji.iwase@prtimes.co.jp>
2024-05-17 09:58:40 +0900

    home.php作成

---
+++ app/View/home.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Article List</title>
+</head>
+<body>
+<h1>Article List</h1>
+
+<!-- New Article Form -->
+<h2>Create New Article</h2>
+<form action="/article" method="POST" enctype="multipart/form-data">
+    <label for="title">Title:</label><br>
+    <input type="text" id="title" name="title" required><br><br>
+
+    <label for="body">Body:</label><br>
+    <textarea id="body" name="body" required></textarea><br><br>
+
+    <label for="thumbnail">Thumbnail Image:</label><br>
+    <input type="file" id="thumbnail" name="thumbnail" required><br><br>
+
+    <label for="images">Additional Images:</label><br>
+    <input type="file" id="images" name="images[]" multiple><br><br>
+
+    <label for="tags">Tags (comma-separated):</label><br>
+    <input type="text" id="tags" name="tags"><br><br>
+
+    <button type="submit">Submit</button>
+</form>
+
+<!-- Article List -->
+<h2>Articles</h2>
+
+<ul>
+    <?php foreach ($articles as $article): ?>
+        <li>
+            <a href="/article?id=<?php echo $article['id']; ?>"><?php echo htmlspecialchars($article['title']); ?></a>
+        </li>
+    <?php endforeach; ?>
+</ul>
+</body>
+</html>
