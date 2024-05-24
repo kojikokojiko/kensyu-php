eab985d248af90e451ea96675bc1d4004a46c63f
koji.iwase <koji.iwase@prtimes.co.jp>
2024-05-24 12:24:25 +0900

    Same-Site：Laxを適用

--- public/index.php
+++ public/index.php
@@ -3,6 +3,12 @@
 use App\App;
 
 require_once __DIR__.'/../vendor/autoload.php';
+
+// PHPセッション設定の変更
+ini_set('session.cookie_samesite', 'Lax');
+//ini_set('session.cookie_secure', 'true'); // HTTPSの場合のみ必要
+ini_set('session.cookie_httponly', 'true');
+
 session_start();
 $app=new App();
 $app->run();
