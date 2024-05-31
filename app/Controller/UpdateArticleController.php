2745359a0faef654cdc6aca63f2172c52a7b1d56
koji.iwase <koji.iwase@prtimes.co.jp>
2024-05-31 14:05:22 +0900

    updatek機能実装

--- app/Controller/UpdateArticleController.php
+++ app/Controller/UpdateArticleController.php
@@ -1,6 +1,7 @@
 <?php
 declare(strict_types=1);
 namespace App\Controller;
+
 use App\Http\Request;
 use App\Http\Response;
 use App\Model\Article;
@@ -8,25 +9,13 @@ use App\Repository\ArticleRepository;
 use InvalidArgumentException;
 use PDO;
 
-/**
- * Class CreateArticleController
- *
- * Controller for handling the creation of new articles.
- *
- * @package App\Controller
- */
 class UpdateArticleController implements ControllerInterface {
+    private int $articleId;
 
-    /**
-     * Invoke action for creating a new article.
-     *
-     * This method processes the request to create a new article, interacts with the repository to save it,
-     * and returns the appropriate response.
-     *
-     * @param Request $req The HTTP request object.
-     * @param PDO $db The database connection object.
-     * @return Response The HTTP response object.
-     */
+    public function __construct(int $articleId) {
+        $this->articleId = $articleId;
+    }
+
     public function __invoke(Request $req, PDO $db): Response {
         $articleRepository = new ArticleRepository($db);
         $title = $req->post['title'];
@@ -34,13 +23,12 @@ class UpdateArticleController implements ControllerInterface {
 
         try{
             $article= new Article(null,$title, $body);
-            $articleId = $articleRepository->createArticle($article);
-            // Redirect to the home page after successful creation
-            return new Response(302, '', ['Location: /']);
+            $rowsUpdated = $articleRepository->updateArticle($title, $body, $this->articleId);
+            return new Response(302, '',['Location: /article/' . $this->articleId]);
 
         }catch (InvalidArgumentException $e){
             $_SESSION['errors'] = [$e->getMessage()];
-            return new Response(302, '', ['Location: /']);
+            return new Response(302, '', ['Location: /article/' . $this->articleId.'/edit']);
         }
     }
 }
