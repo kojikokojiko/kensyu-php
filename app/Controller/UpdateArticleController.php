32d41fac425e608973188fc3e7e18756e43e058d
koji.iwase <koji.iwase@prtimes.co.jp>
2024-06-04 11:11:04 +0900

    フォーマット戻し

--- app/Controller/UpdateArticleController.php
+++ app/Controller/UpdateArticleController.php
@@ -19,37 +19,37 @@ use PDO;
  */
 class UpdateArticleController implements ControllerInterface
 {
-	private int $articleId;
+    private int $articleId;
 
-	public function __construct(int $articleId)
-	{
-		$this->articleId = $articleId;
-	}
+    public function __construct(int $articleId)
+    {
+        $this->articleId = $articleId;
+    }
 
-	/**
-	 * Invoke action for updating an article.
-	 *
-	 * This method processes the request to update an article, interacts with the repository to save it,
-	 * and returns the appropriate response.
-	 *
-	 * @param Request $req The HTTP request object.
-	 * @param PDO $db The database connection object.
-	 * @return Response The HTTP response object.
-	 */
-	public function __invoke(Request $req, PDO $db): Response
-	{
-		$articleRepository = new ArticleRepository($db);
-		$title = $req->post['title'];
-		$body = $req->post['body'];
+    /**
+     * Invoke action for updating an article.
+     *
+     * This method processes the request to update an article, interacts with the repository to save it,
+     * and returns the appropriate response.
+     *
+     * @param Request $req The HTTP request object.
+     * @param PDO $db The database connection object.
+     * @return Response The HTTP response object.
+     */
+    public function __invoke(Request $req, PDO $db): Response
+    {
+        $articleRepository = new ArticleRepository($db);
+        $title = $req->post['title'];
+        $body = $req->post['body'];
 
-		try {
-			$article = new Article($this->articleId, $title, $body);
-			$articleRepository->updateArticle($article);
-			return new Response(302, '', ['Location: /article/' . $this->articleId]);
+        try {
+            $article = new Article($this->articleId, $title, $body);
+            $articleRepository->updateArticle($article);
+            return new Response(302, '', ['Location: /article/' . $this->articleId]);
 
-		} catch (InvalidArgumentException $e) {
-			$_SESSION['errors'] = [$e->getMessage()];
-			return new Response(302, '', ['Location: /article/' . $this->articleId . '/edit']);
-		}
-	}
+        } catch (InvalidArgumentException $e) {
+            $_SESSION['errors'] = [$e->getMessage()];
+            return new Response(302, '', ['Location: /article/' . $this->articleId . '/edit']);
+        }
+    }
 }
