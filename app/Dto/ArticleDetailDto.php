18c4aad6e1ed162dda300152096e25f1ab4d3f2f
koji.iwase <koji.iwase@prtimes.co.jp>
2024-06-17 17:15:47 +0900

    DetailDto変更

--- app/Dto/ArticleDetailDto.php
+++ app/Dto/ArticleDetailDto.php
@@ -12,13 +12,14 @@ namespace App\Dto;
 class ArticleDetailDto
 {
     public function __construct(
-        public int    $articleId,
-        public string $title,
-        public string $body,
-        public int    $userId,
-        public string $userName,
+        public int     $articleId,
+        public string  $title,
+        public string  $body,
+        public int     $userId,
+        public string  $userName,
         public ?string $thumbnailPath,
-        public array  $categories
+        public array   $categories,
+        public array   $imagePaths
     )
     {
     }
